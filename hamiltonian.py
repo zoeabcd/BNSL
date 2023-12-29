b0753611d07b9216684e3c7ff17f2563d4c76f49
Kairui Ding <101644141+USAfucker@users.noreply.github.com>
2023-12-29 12:13:53 +0800

    Update hamiltonian.py with delta_limits

--- hamiltonian.py
+++ hamiltonian.py
@@ -55,6 +55,66 @@ def H_score(n, m, D, d, y, r):
     res = simplify(expand(res))
     return res
 
+def calculate_Delta_ji(n, m, D):
+    Delta_ji = np.zeros((n,n))
+    if m==1:
+        for i in range(n):
+            for j in range(n):
+                Delta_ji[i, j] = -omega(i, [j], D)
+    elif m==2:
+        for i in range(n):
+            for j in range(n):
+                Delta_ji[i, j] = -omega(i, [j], D) - sum(min(0, omega(i, [j, k], D)) for k in range(n) if k != i and k != j)
+    else:
+        for i in range(n):
+            for j in range(n):
+                sum_ = 0
+                # Generate all subsets of size up to m-2 to ensure |J| < m-1
+                for J in itertools.chain.from_iterable(itertools.combinations(range(n), r) for r in range(m-1)):
+                    # Convert tuple to list and ensure i, j not in J
+                    J = list(J)
+                    if i not in J and j not in J:
+                        # Calculate omega with J union {j}
+                        sum_ += min(0, omega(i, J + [j], D))
+                Delta_ji[i, j] = -sum_
+    for i in range(n):
+        for j in range(n):
+            Delta_ji[i, j] = max(0, Delta_ji[i, j])
+    return Delta_ji
+
+def generate_delta(Delta_ji):
+    # Initialize the delta arrays
+    n = Delta_ji.shape[0]
+    delta_max_i = np.zeros(n)
+    delta_consist_ij = np.zeros((n, n))
+    delta_trans_ijk = np.zeros((n, n, n))
+
+    # Calculate delta_max for each i
+    for i in range(n):
+        delta_max_i[i] = max(Delta_ji[i, j] for j in range(n) if j != i)
+
+    # Calculate delta_consist for each pair (i, j)
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                delta_consist_ij[i, j] = (n - 2) * max(Delta_ji[i, k] for k in range(n) if k != i and k != j)
+
+    # Calculate delta_trans for each triplet (i, j, k)
+    # Given delta_trans_ijk = delta_trans > max Delta_ij' for all i != j'
+    # We interpret this as delta_trans_ijk being a constant value for all i, j, k
+    # where i < j < k, which is greater than any Delta_ij' where i != j'
+    # Here, we use a conservative approach by taking the max of all Delta_ij' where i != j'
+    delta_trans = max(max(Delta_ji[i, j] for j in range(n) if i != j) for i in range(n))
+
+    # Assign delta_trans to every element in the delta_trans_ijk array
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                if i < j < k:
+                    delta_trans_ijk[i, j, k] = delta_trans
+
+    return delta_max_i, delta_consist_ij, delta_trans_ijk
+    
 def H_max(n, m, delta_max, d, y, r):
     res = 0
     for i in range(n):
